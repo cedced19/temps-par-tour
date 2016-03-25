8b24c35190562b8370086e0a56c98f474516ddee
Cédric <cedced19@gmail.com>
2016-03-25 16:16:37 +0100

    Add .gitignore and fix bug
    Object are mutables

--- temps-par-tour/www/js/app.js
+++ temps-par-tour/www/js/app.js
@@ -14,31 +14,34 @@ angular.module('TempsParTour', ['ionic'])
     };
 })
 .controller('TempsParTourCtrl', function ($scope, $ionicPopup) {
-  $scope.distanceTurn = 150;
-  $scope.distanceTotal = 825;
-  $scope.time = 4;
+  $scope.default = {
+    distanceTurn: 150,
+    distanceTotal: 825,
+    time: 4
+  };
 
   $scope.calcul = function () {
+    console.log($scope.default .time / ($scope.default.distanceTotal/$scope.default.distanceTurn) * 60)
     var isNumeric = function (n) {
       return !isNaN(parseFloat(n)) && isFinite(n);
     };
-    if (!isNumeric($scope.distanceTurn)) {
+    if (!isNumeric($scope.default.distanceTurn)) {
       $ionicPopup.alert({
          title: 'Erreur',
          template: 'Vous devez donner un nombre pour la disance par tour.'
        });
-    } else if (!isNumeric($scope.distanceTotal)) {
+    } else if (!isNumeric($scope.default.distanceTotal)) {
       $ionicPopup.alert({
          title: 'Erreur',
          template: 'Vous devez donner un nombre pour la disance total.'
        });
-    } else if (!isNumeric($scope.time)) {
+    } else if (!isNumeric($scope.default .time)) {
       $ionicPopup.alert({
          title: 'Erreur',
          template: 'Vous devez donner un nombre pour le temps total.'
        });
     } else {
-      $scope.result = $scope.time / ($scope.distanceTotal/$scope.distanceTurn) * 60;
+        $scope.result = $scope.default.time / ($scope.default.distanceTotal/$scope.default.distanceTurn) * 60;
     }
   };
   $scope.calcul();
